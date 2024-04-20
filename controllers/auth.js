003916f667049a8273d8f0771f2f61a1394cdc67
woragis <masteringthecode.woragis+github@gmail.com>
2024-04-19 21:09:55 -0300

    Add bcrypt encryption to password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,37 @@
 const { generateToken, verifyToken } = require('../jwt')
 const User = require('../models/user')
+const { genSalt, getRounds, hash, compare } = require('bcrypt')
+
+const encryptPassword = async (password) => {
+  const rounds = await getRounds('ass')
+  const salt = await genSalt(rounds)
+  const hashedPassword = await hash(password, salt)
+  return hashedPassword
+}
+
+const decryptPassword = async (originalPassword, encryptedPassword) => {
+  const equal = await compare(
+    originalPassword,
+    encryptPassword,
+    (err, same) => {
+      if (same) {
+        return true
+      } else {
+        return false
+      }
+    }
+  )
+}
 
 const login = async (req, res) => {
   const { email, password } = req.body
   try {
     const user = await User.findOne({ email })
-    if (!user || user.password !== password) {
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid email' })
+    }
+    const rightPassword = decryptPassword(user.password, password)
+    if (!rightPassword) {
       return res.status(401).json({ message: 'Invalid email or password' })
     }
     const token = generateToken({ sub: user._id })
@@ -23,7 +49,8 @@ const register = async (req, res) => {
     if (existingUser) {
       return res.status(400).json({ message: 'Email already registered' })
     }
-    const newUser = new User({ email, password })
+    const encryptedPassword = encryptPassword(password)
+    const newUser = new User({ email, encryptedPassword })
     await newUser.save()
 
     const token = generateToken({ sub: newUser._id })
